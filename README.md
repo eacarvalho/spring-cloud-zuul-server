b7fea59ab01ce5c4deeddd5a3aebe6755ba48d65
Eduardo Alvim de Carvalho <eduardoac@gmail.com>
2016-05-08 14:14:12 -0300

    no message

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# spring-cloud-zuul-server
+# Zuul Server
+
+Run this app as a normal Spring Boot app. If you run from this project 
+it will be on port 8765 (per the `application.yml`). Also run [eureka server](https://github.com/eacarvalho/spring-cloud-eureka-server.git) and the
+[stores](https://github.com/spring-cloud-samples/customers-stores/tree/master/rest-microservices-store) 
+and [spring-cloud-netflix](https://github.com/eacarvalho/spring-cloud-netflix.git).  
+
+You should then be able to view json content from 
+`http://localhost:7070/hello` and `http://eduardo.local:8765/spring-cloud-netflix/hello` which are
+configured in `application.yml` as proxy routes.
---
+++ src/main/java/br/com/iworks/spring/cloud/zuulserver/app/ZuulServerApplication.java
@@ -0,0 +1,21 @@
+package br.com.iworks.spring.cloud.zuulserver.app;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
+@EnableZuulProxy
+public class ZuulServerApplication {
+	
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZuulServerApplication.class).web(true).run(args);
+    }
+
+}
